95b264c43561a363f42cc1b2698abb15622fe3aa
thinpv <thindcna@gmail.com>
2025-04-16 21:57:36 +0700

    change s2 gpio

--- components/platform/esp_io.h
+++ components/platform/esp_io.h
@@ -25,16 +25,16 @@
 
 #if CONFIG_IDF_TARGET_ESP32S2
 /* serial */
-#define GPIO_BOOT_0       2
-#define GPIO_RST_0        3
-#define GPIO_RXD_0        4
-#define GPIO_TXD_0        5
+#define GPIO_BOOT_0       6
+#define GPIO_RST_0        7
+#define GPIO_RXD_0        8
+#define GPIO_TXD_0        9
 
 /* serial */
-#define GPIO_BOOT_1       6
-#define GPIO_RST_1        7
-#define GPIO_RXD_1        8
-#define GPIO_TXD_1        9
+#define GPIO_BOOT_1       10
+#define GPIO_RST_1        11
+#define GPIO_RXD_1        12
+#define GPIO_TXD_1        13
 
 /* leds */
 #define LED_TX          15
